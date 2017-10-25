64dcd9c0bd55ffff49d4f2124e36c1641f188e27
sarafec <fecadusa@gmail.com>
2017-10-24 20:38:55 -0700

    add initial transitions

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function getData(){
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400) {
 			let data = JSON.parse(request.responseText);
-			return createInitialElems(data);
+			refData = data;
+			createInitialElems(data);
 		} else {
 			console.log('error!');
 		}
@@ -26,8 +27,11 @@ let constant = {
 	y: d3.scaleLinear().rangeRound([300 - margin.top - margin.bottom, 0])
 };
 
+let refData;
+
 // kick off chart creation
 function createInitialElems(data) {
+	console.log(data);
 	// create static chart elems
 	let svg =  d3.select('.chart');
 	let g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
@@ -40,6 +44,7 @@ function createInitialElems(data) {
 	
 	// create initial chart
 	createStackChart(data, g);
+
 }
 
 // create visual elements for stack chart
@@ -58,6 +63,10 @@ function createGroupChart(data, g) {
 
 /** STACK CHART METHODS **/
 function createMinorityBar(data, g) {
+	let buildTransition = d3.transition()
+		.duration(1000)
+		.ease(d3.easeLinear);
+
 	// define minority bar stack
 	g.selectAll('.bar1')
 		.data(data.data)
@@ -66,12 +75,17 @@ function createMinorityBar(data, g) {
 		.attr('class', 'bar1')
 		.attr('x', function(d) { return constant.x(d.state); })
 		.attr('y', function(d, i) { return constant.y(evaluateMinorityVal(data, i)); })
+		.style('fill', data.colors[0])
+		.transition(buildTransition)
 		.attr('height', function(d, i) { return constant.height - constant.y(evaluateMinorityVal(data, i)) ;})
-		.attr('width', constant.x.bandwidth())
-		.style('fill', data.colors[0]);
+		.attr('width', constant.x.bandwidth());
 }
 
 function createMajorityBar(data, g) {
+	let buildTransition = d3.transition()
+		.duration(1000)
+		.ease(d3.easeLinear);
+
 	// define majority bar stack
 	g.selectAll('.bar2')
 		.data(data.data)
@@ -80,9 +94,10 @@ function createMajorityBar(data, g) {
 		.attr('class', 'bar2')
 		.attr('x', function(d) { return constant.x(d.state); })
 		.attr('y', function(d, i) { return constant.y(evaluateTotalVal(data, i)); })
+		.style('fill', function(d, i) { return evaluateMajorityColor(data, i); })
+		.transition(buildTransition)
 		.attr('height', function(d, i) { return constant.height - constant.y(evaluateTotalVal(data, i)); })
-		.attr('width', constant.x.bandwidth())
-		.style('fill', function(d, i) { return evaluateMajorityColor(data, i); });
+		.attr('width', constant.x.bandwidth());
 }
 
 /** GROUPED CHART METHODS **/
@@ -92,7 +107,7 @@ function createMaleBar(data, g) {
 		.enter()
 		.append('rect')
 		.attr('class', 'maleBar')
-		.attr('x', function(d) { return constant.x(d.state) + 4; })
+		.attr('x', function(d) { return constant.x(d.state) + 3; })
 		.attr('y', function(d, i) { return constant.y(getMaleVals(data, i)); })
 		.attr('height', function(d, i){ return constant.height - constant.y(getMaleVals(data, i)); })
 		.attr('width', constant.x.bandwidth()/3)
@@ -105,7 +120,7 @@ function createFemaleBar(data, g) {
 		.enter()
 		.append('rect')
 		.attr('class', 'femaleBar')
-		.attr('x', function(d) { return constant.x(d.state); })
+		.attr('x', function(d) { return constant.x(d.state) - 0.25; })
 		.attr('y', function(d, i) { return constant.y(getFemaleVals(data, i)); })
 		.attr('height', function(d, i){ return constant.height - constant.y(getFemaleVals(data, i)); })
 		.attr('width', constant.x.bandwidth()/3)
@@ -118,12 +133,50 @@ function createTotalBar(data, g) {
 		.enter()
 		.append('rect')
 		.attr('class', 'totalBar')
-		.attr('x', function(d) { return constant.x(d.state) + 8; })
+		.attr('x', function(d) { return constant.x(d.state) + 6; })
 		.attr('y', function(d, i) { return constant.y(getTotalVals(data, i)); })
 		.attr('height', function(d, i){ return constant.height - constant.y(getTotalVals(data, i)); })
 		.attr('width', constant.x.bandwidth()/3)
 		.style('fill', data.colors[0]);
 }
+
+/** TRANSITION FUNCTIONS **/
+// if possible, collapse into one reusable transition function
+function transitionToGrouped() {
+	let svg =  d3.select('.chart'),
+		g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+
+	let toGroupedChart = d3.transition()
+		.duration(1000)
+		.ease(d3.easeLinear);
+
+	d3.selectAll('.bar1').transition(toGroupedChart)
+		.attr('width', 0);
+	d3.selectAll('.bar2').transition(toGroupedChart)
+		.attr('width', 0);
+
+	return createGroupChart(refData, g);
+}
+
+function transitionToStacked() {
+	let svg =  d3.select('.chart'),
+		g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+	let toStackedChart = d3.transition()
+		.duration(1000)
+		.ease(d3.easeLinear);
+
+	d3.selectAll('.maleBar').transition(toStackedChart)
+		.attr('width', 0);
+	d3.selectAll('.femaleBar').transition(toStackedChart)
+		.attr('width', 0);
+	d3.selectAll('.totalBar').transition(toStackedChart)
+		.attr('width', 0);
+
+	return createStackChart(refData, g);
+}
+
 
 /** STATIC ELEMENTS **/
 function createScale(data, g) {
@@ -280,13 +333,23 @@ function evaluateMajorityColor(data, val) {
 }
 
 /** USER INTERACTION EVENTS **/
-
+let radioButtons = document.querySelector('.button-group');
+radioButtons.addEventListener('change', function(evt){
+	let chartType = evt.target.defaultValue;
+	if(chartType){
+		if (chartType === 'Grouped'){
+			transitionToGrouped();
+		} else if (chartType === 'Stacked') {
+			transitionToStacked();
+		}
+	}
+});
 
 // kick off xhr
 getData();
 
 //additions
-// - tooltips on hover/focus, extendable
 // - transition between grouped and stacked
-// - table with additional information on senatorsß
+// - tooltips on hover/focus, extendable
+// - table with additional information on senators
 // - call this data from pro publica API
